864bdb7612139ecc3b62c644f29e7413dddab055
jonathanrossoatos <jonathan.rosso@atos.net>
2025-04-10 11:49:59 -0300

    chore: initial project setup with Webpack and React

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello World, 33333333333333333333333333333 123!!!!</h1>,
+    document.getElementById('root')
+);
